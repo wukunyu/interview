8afeb1e018a86b72b6c060ff72e52c9c52826b77
wukunyu <wukunyu@stargraph.cn>
2019-11-04 20:18:32 +0800

    feat: init code

--- testdoc/groups/urls.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-__author__ = 'xwchen2'
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('groups.views',
-    url(r'^$', 'home'),
-    url(r'^group$', 'group'),
-)
